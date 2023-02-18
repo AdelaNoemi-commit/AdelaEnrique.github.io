d7a28e555166b3a28cb88d766802bb115a1396e5
Adela Noemi <noemy2adel@gmail.com>
2023-02-18 01:02:59 +0000

    Este es mi primer proyecto en commit

---
+++ js/junction.js
@@ -0,0 +1,26 @@
+document.getElementById('usuario').onclick= function onclick() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("user").innerHTML ="User: Neri234"}
+
+document.getElementById('aniversario').onclick= function onclick() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("fecha").innerHTML ="25/04/1990"}
+
+document.getElementById('dirección').onclick= function onclick() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("location").innerHTML ="Argetina, Buenos Aires, La Plara1064"}
+
+document.getElementById('movil').onclick= function onclick() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("celular").innerHTML = "54-011-205567"}
+
+document.getElementById('correo').onclick= function onclick() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("email").innerHTML = "Andi.Martinez123@gmail.com"}
+document.getElementById('button').onclick = function onclick1() {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo1").innerHTML = "Lider de reseñas y mentora del área de criticas en la Editorial King of Kings";
+}
+document.getElementById('boton').onclick= function onclick2() { 
+    console.log ("Capturamos el evento click");
+document.getElementById("demo2").innerHTML ="Facilidad en redacción de contenido y edición de textos. Conocimiento de los géneros literarios ";}
